e8bfef6df6b3f25d8f56dde28503c9671a34bca3
Mike <m.a.ilnitsky@yandex.ru>
2026-01-08 00:20:37 +0700

    Created Solution

---
+++ Ilnitsky.PollsA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ilnitsky.PollsA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
